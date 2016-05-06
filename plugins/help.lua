01c4ef4fca05187baaad2dbfae43bfe373311873
nort-ir <Develop@nort.ir>
2016-05-06 11:27:17 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,1 +1,30 @@
+local function run(msg, matches)
+if is_momod(msg) and msg.to.type == "" then        
+  local text = [[
+💠 دستورات ربات ضد اسپم نورت 💠
 
+🔹لیست راهنمای پلاگین ها 
+/helps
+
+🔸لیست راهنمای کلی ربات 
+/help_all
+
+🔹 لیست راهنمای خصوصی 
+/help_pv
+
+------------------------------
+@nortteam Anti Spam Nort
+]]
+    send_msg("chat#id"..msg.to.id, text, ok_cb, false)
+  end
+end 
+return {
+  description = "Help owner.  ", 
+  usage = {
+    "ownerhelp: Show help for owners.",
+  },
+  patterns = {
+    "^([!/#]help)$",
+  }, 
+  run = run,
+}
