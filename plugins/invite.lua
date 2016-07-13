07655c559f1d8ae9fbc288fd1fbe946dd1e8b9bb
WebShark025 <arshshia@gmail.com>
2016-07-13 15:43:34 +0430

    Translated.

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -5,11 +5,11 @@ local function callbackres(extra, success, result)
 	local chat = 'chat#id'..extra.chatid
 	local channel = 'channel#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then 
-        send_large_msg(chat, 'User is banned.')
-        send_large_msg(channel, 'User is banned.')
+        send_large_msg(chat, 'یوزر بن شده است')
+        send_large_msg(channel, 'یوزر بن شده است')
 	elseif is_gbanned(result.id) then
-	    send_large_msg(chat, 'User is globaly banned.')
-		send_large_msg(channel, 'User is globaly banned.')
+	    send_large_msg(chat, 'یوزر گلوبال بن شده است')
+		send_large_msg(channel, 'یوزر گلوبال بن شده است')
 	else    
 	    chat_add_user(chat, user, ok_cb, false) 
 		channel_invite(channel, user, ok_cb, false)
@@ -21,11 +21,11 @@ function run(msg, matches)
 	return
   end
   if not is_admin1(msg) then -- For admins only !
-		return 'Only admins can invite.'
+		return 'فقط ادمین ها امکان دعوت را دارند'
   end
   if not is_realm(msg) then
     if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin1(msg) then
-		  return 'Group is private.'
+		  return 'گروه خصوصیست'
     end
   end
 	if msg.to.type ~= 'chat' or msg.to.type ~= 'channel' then 
